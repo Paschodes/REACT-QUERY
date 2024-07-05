6df12e561f57d9f36eac52a0485effbb1c0bb4b7
Paschodes <paschal.c.chikwendu@gmail.com>
2024-07-05 16:03:39 +0100

    done

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,7 +1,9 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-const initialUrl = "https://swapi.dev/api/people/";
+const baseUrl = "https://swapi-node.vercel.app";
+const initialUrl = baseUrl + "/api/people";
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
@@ -9,5 +11,39 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage, isFetching, isLoading, isError } = useInfiniteQuery({
+    queryKey: ["sw-people"],
+    queryFn: ({pageParam = initialUrl}) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => {
+      return lastPage.next ? baseUrl + lastPage.next : undefined;
+    },
+  });
+
+  if (isLoading) {
+    return <div className="loading">Loading o...</div>
+  }
+  if (isError) {
+    return <div>Error o! {Error.toString()}</div>
+  }
+
+  return (
+    <>
+    {/* //to indicate to de user dat more data is loading as dey scroll towards de page end */}
+    {isFetching && <div className="loading">chill...loading...</div>}   
+    
+    <InfiniteScroll initialLoad={false} loadMore={() => {
+      if (!isFetching) {
+        fetchNextPage()
+      }
+    }} hasMore={hasNextPage}>
+      {data.pages.map((pageData) => {
+        return pageData.results.map((person) => {
+          return (
+            <Person key={person.fields.name} name={person.fields.name} hairColor={person.fields.hair_color} eyeColor={person.fields.eye_color}/>
+          );
+        });
+      })}
+    </InfiniteScroll>
+    </>
+  );
 }
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,7 +1,9 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-const initialUrl = "https://swapi.dev/api/species/";
+const baseUrl = "https://swapi-node.vercel.app";
+const initialUrl = baseUrl + "/api/species";
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
@@ -9,5 +11,38 @@ const fetchUrl = async (url) => {
 
 export function InfiniteSpecies() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {data, fetchNextPage, hasNextPage, isFetching, isLoading, isError} = useInfiniteQuery({
+    queryKey: ["sw-people"],
+    queryFn: ({pageParam = initialUrl}) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => {
+      return lastPage.next ? baseUrl + lastPage.next : undefined;
+    },
+  });
+
+  if (isLoading) {
+    return <div className="loading">Loading eeh...</div>
+  }
+
+  if (isError) {
+    return <div className="loading">Error eeh...{Error.toString()}</div>
+  }
+
+  return (
+    <>
+    {isFetching && <div className="loading">tuning in...</div>}
+      <InfiniteScroll initialLoad={false} hasMore={hasNextPage} loadMore={() => {
+        if (!isFetching) {
+          fetchNextPage()
+        }
+      }}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((specie) => {
+            return (
+              <Species key={specie.fields.name} name={specie.fields.name} hairColor={specie.fields.hair_colors} eyeColor={specie.fields.eye_colors}/>
+            )
+          })
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
